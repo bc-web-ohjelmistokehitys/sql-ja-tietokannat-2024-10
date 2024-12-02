41a76f1aa6d3f637b9ca666867247468b7e1f0f3
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2024-12-02 09:43:53 +0200

    Exercises

---
+++ tuntimuistiinpanot/tunti-06/csharp-harjoitus/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+
+var connectionString = "Host=localhost;Database=suomioy";
+
+await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+// Insert some data
+/*
+await using (var cmd = dataSource.CreateCommand("INSERT INTO data (some_field) VALUES ($1)"))
+{
+    cmd.Parameters.AddWithValue("Hello world");
+    await cmd.ExecuteNonQueryAsync();
+}
+*/
+
+// Retrieve all rows
+
+try {
+
+    var personId = "1";
+
+    await using var cmd = dataSource.CreateCommand("SELECT last_name FROM person WHERE id = @id LIMIT 1");
+    // cmd.Parameters.AddWithValue("$1", personId);
+
+    cmd.Parameters.Add("@id", NpgsqlTypes.NpgsqlDbType.Integer).Value = int.Parse(personId);
+
+    await using var reader = await cmd.ExecuteReaderAsync();
+    while (await reader.ReadAsync())
+    {
+        Console.WriteLine(reader.GetString(0));
+    }
+
+} catch (PostgresException e) {
+
+    Console.WriteLine("OH NOES KAUHEA VIRHE");
+    
+    Console.WriteLine(e.ToString());
+} finally {
+    Console.WriteLine("Minut ajetaan vaikka mitä tapahtuisi");
+}
+
---
+++ tuntimuistiinpanot/tunti-06/nodejs-harjoitus/index.js
@@ -0,0 +1,20 @@
+import pg from "pg";
+
+const { Client } = pg;
+
+const client = new Client({
+  connectionString: "postgres://localhost/suomioy",
+});
+
+const muuttuja: number = "5";
+
+console.log("hellurei");
+await client.connect();
+
+const ret = await client.query("SELECT * FROM person LIMIT 10");
+
+console.log("ROWS", ret.rows);
+
+console.log("hellurei2");
+
+await client.end();
